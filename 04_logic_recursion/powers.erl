1eaa47ae95053877d59fce3f862b06983c1db748
Michael Ries <michael@riesd.com>
2013-08-02 15:45:51 -0600

    4-5 passing

--- 04_logic_recursion/powers.erl
+++ 04_logic_recursion/powers.erl
@@ -1,5 +1,5 @@
 -module(powers).
--export([raise/2]).
+-export([raise/2, nth_root/2]).
 -include_lib("eunit/include/eunit.hrl").
 
 %% raise/2
@@ -24,4 +24,25 @@ raise_test_() -> [
   ?_assertEqual( 0.125, raise(2,-3)),
   ?_assertEqual( 1.0, raise(1,-4)),
   ?_assertEqual( 1, raise(1,10000000000))
+].
+
+%% nth_root/2
+-spec(nth_root(number(), integer()) -> number()).
+nth_root(X, N) -> nth_root(X, N, X / 2.0).
+
+%% nth_root/3
+nth_root(X, N, A) ->
+  io:format("Current guess is ~p~n", [A]),
+  F = raise(A,N) - X,
+  Fprime = N * raise(A, N-1),
+  Next = A - F / Fprime,
+  Change = abs(Next - A),
+  if Change < 1.0e-8 -> Next;
+    true -> nth_root(X, N, Next)
+  end.
+
+
+nth_root_test_() -> [
+  ?_assertEqual( 6.0, nth_root(36, 2)),
+  ?_assertEqual( 3.0, nth_root(27, 3))
 ].
