838a088cd0cce753ae8ef083f12461c26f860fb6
Michael Ries <michael@riesd.com>
2013-08-02 15:23:40 -0600

    added special clause for 1 being raised to a power. Tested deep recursion and could not crash the vm.

--- 04_logic_recursion/powers.erl
+++ 04_logic_recursion/powers.erl
@@ -5,6 +5,7 @@
 -spec(raise(number(), number()) -> number()).
 raise(_Base, 0) -> 1;
 raise(Base, 1) -> Base;
+raise(1, _Power) -> 1;
 raise(Base, Power) when Power > 1 -> Base * raise(Base, Power - 1);
 raise(Base, Power) when Power < 0 -> 1.0 / raise(Base, -Power).
 
@@ -14,5 +15,7 @@ raise_test_() -> [
   ?_assertEqual( 8, raise(2,3)),
   ?_assertEqual( -8, raise(-2,3)),
   ?_assertEqual( 1.728, raise(1.2, 3)),
-  ?_assertEqual( 0.125, raise(2,-3))
+  ?_assertEqual( 0.125, raise(2,-3)),
+  ?_assertEqual( 1, raise(1,-4)),
+  ?_assertEqual( 1, raise(1,10000000000))
 ].
