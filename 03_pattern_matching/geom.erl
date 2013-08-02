6c31a600538e46c828c0c3ff7178a84afedcbef7
Michael Ries <michael@riesd.com>
2013-08-02 14:26:18 -0600

    passing 3-3

--- 03_pattern_matching/geom.erl
+++ 03_pattern_matching/geom.erl
@@ -3,9 +3,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -spec(area(atom(), number(), number()) -> number()).
-area(rectangle, Width, Height) -> Width * Height;
-area(triangle, Base, Height) -> Base * Height / 2.0;
-area(ellipse, Width, Height) -> math:pi() * Width * Height.
+area(rectangle, Width, Height) when Width >= 0, Height >= 0 ->
+  Width * Height;
+area(triangle, Base, Height) when Base >= 0, Height >= 0 ->
+  Base * Height / 2.0;
+area(ellipse, Width, Height) when Width >= 0, Height >= 0 ->
+  math:pi() * Width * Height.
 
 
 area_test_() -> [
@@ -13,5 +16,6 @@ area_test_() -> [
   ?_assert( area(rectangle, 0,4)   =:= 0   ),
   ?_assert( area(rectangle, 1.5,4) =:= 6.0 ),
   ?_assert( area(triangle, 3,5)    =:= 7.5 ),
-  ?_assert( area(ellipse, 2,4) > 25.132 andalso area(ellipse, 2,4) < 25.133)
+  ?_assert( area(ellipse, 2,4) > 25.132 andalso area(ellipse, 2,4) < 25.133),
+  ?_assertException(error, function_clause, area(rectangle, -1, -1))
 ].
