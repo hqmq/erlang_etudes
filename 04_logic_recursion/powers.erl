8040f63c3584e518eb6bf2855e830241fbf6b686
Michael Ries <michael@riesd.com>
2013-08-02 15:31:44 -0600

    4-4 passing

--- 04_logic_recursion/powers.erl
+++ 04_logic_recursion/powers.erl
@@ -2,12 +2,18 @@
 -export([raise/2]).
 -include_lib("eunit/include/eunit.hrl").
 
+%% raise/2
 -spec(raise(number(), number()) -> number()).
 raise(_Base, 0) -> 1;
-raise(Base, 1) -> Base;
-raise(1, _Power) -> 1;
-raise(Base, Power) when Power > 1 -> Base * raise(Base, Power - 1);
-raise(Base, Power) when Power < 0 -> 1.0 / raise(Base, -Power).
+raise(Base, Power) when Power > 0 ->
+  raise(Base, Power, 1);
+raise(Base, Power) when Power < 0 ->
+  1.0 / raise(Base, -Power).
+
+%% raise/3
+raise(1, _Power, _Acc) -> 1;
+raise(_Base, 0, Acc) -> Acc;
+raise(Base, Power, Acc) -> raise(Base, Power -1, Base * Acc).
 
 raise_test_() -> [
   ?_assertEqual( 1, raise(4.256, 0)),
@@ -16,6 +22,6 @@ raise_test_() -> [
   ?_assertEqual( -8, raise(-2,3)),
   ?_assertEqual( 1.728, raise(1.2, 3)),
   ?_assertEqual( 0.125, raise(2,-3)),
-  ?_assertEqual( 1, raise(1,-4)),
+  ?_assertEqual( 1.0, raise(1,-4)),
   ?_assertEqual( 1, raise(1,10000000000))
 ].
