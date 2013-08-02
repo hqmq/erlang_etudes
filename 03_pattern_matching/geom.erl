35beb95a39ccdc27b8e25cc13e279bc4f0179390
Michael Ries <michael@riesd.com>
2013-08-02 14:31:55 -0600

    3-4 passing

--- 03_pattern_matching/geom.erl
+++ 03_pattern_matching/geom.erl
@@ -1,6 +1,10 @@
 -module(geom).
--export([area/3]).
+-export([area/1]).
 -include_lib("eunit/include/eunit.hrl").
+
+
+-spec(area({atom(), number(), number()}) -> number()).
+area({Type, A, B}) -> area(Type, A, B).
 
 -spec(area(atom(), number(), number()) -> number()).
 area(rectangle, Width, Height) when Width >= 0, Height >= 0 ->
@@ -13,11 +17,11 @@ area(_, _Width, _Height) -> 0.
 
 
 area_test_() -> [
-  ?_assert( area(rectangle, 3,4)   =:= 12  ),
-  ?_assert( area(rectangle, 0,4)   =:= 0   ),
-  ?_assert( area(rectangle, 1.5,4) =:= 6.0 ),
-  ?_assert( area(triangle, 3,5)    =:= 7.5 ),
-  ?_assert( area(ellipse, 2,4) > 25.132 andalso area(ellipse, 2,4) < 25.133),
-  ?_assert( area(rectangle, -1,1)  =:= 0 ),
-  ?_assert( area(pentagon, 1, 1)   =:= 0 )
+  ?_assert( area({rectangle, 3,4})   =:= 12  ),
+  ?_assert( area({rectangle, 0,4})   =:= 0   ),
+  ?_assert( area({rectangle, 1.5,4}) =:= 6.0 ),
+  ?_assert( area({triangle, 3,5})    =:= 7.5 ),
+  ?_assert( area({ellipse, 2,4}) > 25.132 andalso area({ellipse, 2,4}) < 25.133),
+  ?_assert( area({rectangle, -1,1})  =:= 0 ),
+  ?_assert( area({pentagon, 1, 1})   =:= 0 )
 ].
