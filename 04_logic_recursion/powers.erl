ddbee66bad3b1dbadaeac1e477c9d55b2c4250c0
Michael Ries <michael@riesd.com>
2013-08-02 15:15:14 -0600

    4-3 passing

---
+++ 04_logic_recursion/powers.erl
@@ -0,0 +1,18 @@
+-module(powers).
+-export([raise/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec(raise(number(), number()) -> number()).
+raise(_Base, 0) -> 1;
+raise(Base, 1) -> Base;
+raise(Base, Power) when Power > 1 -> Base * raise(Base, Power - 1);
+raise(Base, Power) when Power < 0 -> 1.0 / raise(Base, -1 * Power).
+
+raise_test_() -> [
+  ?_assertEqual( 1, raise(4.256, 0)),
+  ?_assertEqual( 5, raise(5,1)),
+  ?_assertEqual( 8, raise(2,3)),
+  ?_assertEqual( -8, raise(-2,3)),
+  ?_assertEqual( 1.728, raise(1.2, 3)),
+  ?_assertEqual( 0.125, raise(2,-3))
+].
