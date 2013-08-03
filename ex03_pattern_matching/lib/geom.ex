29b39d8adeb0973a84f71511d12478452f27625e
Michael Ries <michael@riesd.com>
2013-08-02 21:52:09 -0600

    ex3-2 passing

--- ex03_pattern_matching/lib/geom.ex
+++ ex03_pattern_matching/lib/geom.ex
@@ -4,15 +4,15 @@ defmodule Geom do
     """
 
   @doc "area(shape, width, height) -> the area of a rectange with the given width and height."
-  def area(:rectangle, width, height) do
+  def area(:rectangle, width, height) when width > 0 and height > 0 do
     (width * height)
   end
 
-  def area(:triangle, base, height) do
+  def area(:triangle, base, height) when base > 0 and height > 0 do
     (base * height)/2.0
   end
 
-  def area(:ellipse, major, minor) do
+  def area(:ellipse, major, minor) when major > 0 and minor > 0 do
     :math.pi() * major * minor
   end
 end
--- ex03_pattern_matching/test/geom_test.exs
+++ ex03_pattern_matching/test/geom_test.exs
@@ -9,5 +9,8 @@ defmodule GeomTest do
     assert Geom.area(:triangle, 3,5) == 7.5
     ellipse = Geom.area(:ellipse, 2, 4)
     assert ellipse > 25.132 and ellipse < 25.133
+    assert_raise FunctionClauseError, fn ->
+      Geom.area(:ellipse, -2, -4) > 25.132
+    end
   end
 end
