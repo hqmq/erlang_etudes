1b8a3b5ae3ce2c6b9e7288410210c7bca914ae55
Michael Ries <michael@riesd.com>
2013-08-02 21:57:33 -0600

    ex3-4 passing

--- ex03_pattern_matching/lib/geom.ex
+++ ex03_pattern_matching/lib/geom.ex
@@ -3,20 +3,25 @@ defmodule Geom do
     Calculates the area of geometric shapes.
     """
 
-  @doc "area(shape, width, height) -> the area of a rectange with the given width and height."
-  def area(:rectangle, width, height) when width > 0 and height > 0 do
+  @doc "area({shape, width, height}) -> the area of a shape with the given width and height.
+  supported shapes = rectangle, triangle, ellipse"
+  def area({shape, width, height}) do
+    area(shape, width, height)
+  end
+
+  defp area(:rectangle, width, height) when width > 0 and height > 0 do
     (width * height)
   end
 
-  def area(:triangle, base, height) when base > 0 and height > 0 do
+  defp area(:triangle, base, height) when base > 0 and height > 0 do
     (base * height)/2.0
   end
 
-  def area(:ellipse, major, minor) when major > 0 and minor > 0 do
+  defp area(:ellipse, major, minor) when major > 0 and minor > 0 do
     :math.pi() * major * minor
   end
 
-  def area(_shape, _a, _b) do
+  defp area(_shape, _a, _b) do
     0
   end
 end
