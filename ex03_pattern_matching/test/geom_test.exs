36848ebcde7c95b3a366479e7d369a230227675a
Michael Ries <michael@riesd.com>
2013-08-02 21:48:52 -0600

    refactor test

--- ex03_pattern_matching/test/geom_test.exs
+++ ex03_pattern_matching/test/geom_test.exs
@@ -4,10 +4,10 @@ defmodule GeomTest do
   use ExUnit.Case, async: true
 
   test "the Geom.area/2" do
-    assert(Geom.area(:rectangle,3,4) == 12)
-    assert(Geom.area(:rectangle,12,7) == 84)
-    assert(Geom.area(:triangle, 3,5) == 7.5)
+    assert Geom.area(:rectangle,3,4) == 12
+    assert Geom.area(:rectangle,12,7) == 84
+    assert Geom.area(:triangle, 3,5) == 7.5
     ellipse = Geom.area(:ellipse, 2, 4)
-    assert(ellipse > 25.132 and ellipse < 25.133)
+    assert ellipse > 25.132 and ellipse < 25.133
   end
 end
